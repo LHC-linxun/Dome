dc82ed7ecf9d140ce4ebbc70f4f85781c5c77252
LHC-linxun <56421992+LHC-linxun@users.noreply.github.com>
2022-05-31 02:23:08 +0800

    Revert "提交测试"
    This reverts commit 5d7c616b20ceeebbb84df7592f068b3643182757.

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -10,7 +10,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("7777777");
+        
     }
 
     // Update is called once per frame
